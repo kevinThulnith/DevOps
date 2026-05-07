312d9c142a5e474982ca700b597064e494c3d847
Kevin <112649346+kevinThulnith@users.noreply.github.com>
2026-05-07 21:33:26 +0530

    update backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,6 +94,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "backend.middleware.HealthCheckMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
