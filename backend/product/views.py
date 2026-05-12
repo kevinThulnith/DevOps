d4a57e281342d7d9bececbe95122d0bccbc95b4c
Kevin <112649346+kevinThulnith@users.noreply.github.com>
2026-05-12 10:44:30 +0530

    add backend caching with redis

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,5 +1,5 @@
+from core.cache_utils import RBACCacheMixin, TIMEOUT_LONG, invalidate_resource
 from .serializers import ProductSerializer, ProductProcessSerializer
-from rest_framework.viewsets import ModelViewSet
 from django.shortcuts import get_object_or_404
 from .permissions import ProductPermission
 from .models import Product
@@ -7,7 +7,7 @@ from .models import Product
 # TODO: Create product model views
 
 
-class ProductViewSet(ModelViewSet):
+class ProductViewSet(RBACCacheMixin):
     """
     Product API
     - Admins: Full CRUD access
@@ -18,9 +18,12 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     permission_classes = [ProductPermission]
     queryset = Product.objects.all()
+    cache_resource = "product"
+    cache_timeout = TIMEOUT_LONG
+    cache_scope = "global"
 
 
-class ProductProcessViewSet(ModelViewSet):
+class ProductProcessViewSet(RBACCacheMixin):
     """
     Product Process API
     - Must be accessed through a product
@@ -28,6 +31,9 @@ class ProductProcessViewSet(ModelViewSet):
 
     serializer_class = ProductProcessSerializer
     permission_classes = [ProductPermission]
+    cache_resource = "product_process"
+    cache_timeout = TIMEOUT_LONG
+    cache_scope = "global"
 
     def get_queryset(self):
         product_pk = self.kwargs.get("product_pk")
@@ -42,3 +48,4 @@ class ProductProcessViewSet(ModelViewSet):
         product_pk = self.kwargs.get("product_pk")
         product = get_object_or_404(Product, pk=product_pk)
         serializer.save(product=product)
+        invalidate_resource(self.cache_resource)
