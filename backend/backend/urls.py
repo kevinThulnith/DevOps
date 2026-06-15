c243973d4abf74683956d2a9885295b3afec0e2a
Kevin <112649346+kevinThulnith@users.noreply.github.com>
2026-06-15 21:59:47 +0530

    update setup

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("api-auth/", include("rest_framework.urls")),
+    path("", include("django_prometheus.urls")),  # ? exposes /metrics at root
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh_token"),
     path("api/token/blacklist/", TokenBlacklistView.as_view(), name="blacklist_token"),
