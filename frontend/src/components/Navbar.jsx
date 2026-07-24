aa1c554938f7d653b06a7f55a157227ccd95bc3b
Kevin <112649346+kevinThulnith@users.noreply.github.com>
2026-07-24 23:41:57 +0530

    frontend fix navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -107,13 +107,13 @@ function Navbar() {
         />
       )}
 
-      {/* Side bar */}
+      {/* Sidebar */}
       <div
         className={`
             fixed top-14 right-0 h-[calc(100vh-3.5rem)] w-full sm:w-80 bg-card-main shadow-2xl
             transform transition-transform duration-300 ease-in-out
             ${isMenuOpen ? "translate-x-0" : "translate-x-full"}
-            flex flex-col p-4 z-50 
+            flex flex-col p-4 z-50
           `}
       >
         {/* Scrollable section */}
