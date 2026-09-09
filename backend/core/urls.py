0aa4c293cdea22b699e00b6120d64d8d4ed13efa
Kevin <112649346+kevinThulnith@users.noreply.github.com>
2026-09-09 14:39:57 +0530

    backend configure JWT | frontend debug logout

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,5 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
+
 from .views import (
     MachineOperatorAssignmentViewSet,
     DepartmentViewSet,
--- backend/labor/urls.py
+++ backend/labor/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 from .views import LaborAllocationViewSet, SkillMatrixViewSet
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
 
 router = DefaultRouter()
 router.register(r"allocation", LaborAllocationViewSet, basename="labor-allocation")
